e3ac1e7daa734feda062020d49a65aeef9f1941d
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-09-21 22:52:51 +0200

    Added recipe for duplicating circuits

--- src/main/java/net/replaceitem/integratedcircuit/IntegratedCircuit.java
+++ src/main/java/net/replaceitem/integratedcircuit/IntegratedCircuit.java
@@ -6,6 +6,9 @@ import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityT
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.Item;
+import net.minecraft.recipe.BookCloningRecipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.util.registry.Registry;
 import net.replaceitem.integratedcircuit.network.ServerPacketHandler;
 import net.replaceitem.integratedcircuit.network.packet.ComponentInteractionC2SPacket;
@@ -16,12 +19,14 @@ public class IntegratedCircuit implements ModInitializer {
 
     public final static String MOD_ID = "integrated_circuit"; // TODO - maybe make this dynamic
     public final static Block INTEGRATED_CIRCUIT_BLOCK = new IntegratedCircuitBlock();
-    private static final Item INTEGRATED_CIRCUIT_ITEM = new IntegratedCircuitItem();
+    public static final Item INTEGRATED_CIRCUIT_ITEM = new IntegratedCircuitItem();
     public static final BlockEntityType<IntegratedCircuitBlockEntity> INTEGRATED_CIRCUIT_BLOCK_ENTITY = Registry.register(
             Registry.BLOCK_ENTITY_TYPE,
             new IntegratedCircuitIdentifier("integrated_circuit_block_entity"),
             FabricBlockEntityTypeBuilder.create(IntegratedCircuitBlockEntity::new, INTEGRATED_CIRCUIT_BLOCK).build()
     );
+
+    public static final SpecialRecipeSerializer<IntegratedCircuitCloningRecipe> CIRCUIT_CLONING = RecipeSerializer.register("integrated_circuit:crafting_special_circuit_cloning", new SpecialRecipeSerializer<IntegratedCircuitCloningRecipe>(IntegratedCircuitCloningRecipe::new));
 
 
     @Override
---
+++ src/main/java/net/replaceitem/integratedcircuit/IntegratedCircuitCloningRecipe.java
@@ -0,0 +1,96 @@
+package net.replaceitem.integratedcircuit;
+
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.SpecialCraftingRecipe;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+
+public class IntegratedCircuitCloningRecipe extends SpecialCraftingRecipe {
+    public IntegratedCircuitCloningRecipe(Identifier identifier) {
+        super(identifier);
+    }
+
+    @Override
+    public boolean matches(CraftingInventory craftingInventory, World world) {
+        int sourceIndex = -1;
+        int destinationIndex = -1;
+        for (int i = 0; i < craftingInventory.size(); i++) {
+            ItemStack stack = craftingInventory.getStack(i);
+            if(stack.isEmpty()) continue;
+            if(!stack.isOf(IntegratedCircuit.INTEGRATED_CIRCUIT_ITEM)) return false;
+            if(sourceIndex == -1 && stack.getCount() == 1 && stack.hasNbt() && stack.getNbt().contains("BlockEntityTag")) {
+                sourceIndex = i;
+                continue;
+            }
+            if(destinationIndex == -1 && !stack.hasNbt()) {
+                destinationIndex = i;
+                continue;
+            }
+            return false;
+        }
+        
+        return sourceIndex != -1 && destinationIndex != -1;
+    }
+
+    @Override
+    public ItemStack craft(CraftingInventory craftingInventory) {
+        int sourceIndex = -1;
+        int destinationIndex = -1;
+        for (int i = 0; i < craftingInventory.size(); i++) {
+            ItemStack stack = craftingInventory.getStack(i);
+            if(stack.isEmpty()) continue;
+            if(!stack.isOf(IntegratedCircuit.INTEGRATED_CIRCUIT_ITEM)) return ItemStack.EMPTY;
+            if(sourceIndex == -1 && stack.getCount() == 1 && stack.hasNbt() && stack.getNbt().contains("BlockEntityTag")) {
+                sourceIndex = i;
+                continue;
+            }
+            if(destinationIndex == -1 && !stack.hasNbt()) {
+                destinationIndex = i;
+                continue;
+            }
+            return ItemStack.EMPTY;
+        }
+        ItemStack sourceStack = craftingInventory.getStack(sourceIndex);
+        ItemStack craftedStack = IntegratedCircuit.INTEGRATED_CIRCUIT_ITEM.getDefaultStack();
+        craftedStack.setNbt(sourceStack.getNbt().copy());
+        return craftedStack;
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getRemainder(CraftingInventory craftingInventory) {
+        DefaultedList<ItemStack> remainder = DefaultedList.ofSize(craftingInventory.size(), ItemStack.EMPTY);
+        int sourceIndex = -1;
+        int destinationIndex = -1;
+        for (int i = 0; i < craftingInventory.size(); i++) {
+            ItemStack stack = craftingInventory.getStack(i);
+            if(stack.isEmpty()) continue;
+            if(!stack.isOf(IntegratedCircuit.INTEGRATED_CIRCUIT_ITEM)) return remainder;
+            if(sourceIndex == -1 && stack.getCount() == 1 && stack.hasNbt() && stack.getNbt().contains("BlockEntityTag")) {
+                sourceIndex = i;
+                continue;
+            }
+            if(destinationIndex == -1 && !stack.hasNbt()) {
+                destinationIndex = i;
+                continue;
+            }
+            return remainder;
+        }
+        
+        remainder.set(sourceIndex, craftingInventory.getStack(sourceIndex).copy());
+        
+        return remainder;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return IntegratedCircuit.CIRCUIT_CLONING;
+    }
+
+    @Override
+    public boolean fits(int width, int height) {
+        return width >= 3 && height >= 3;
+    }
+}
