6c4b61a83c7bc27c7f00b0a00f39af5c8cfbf489
replaceitem <40722305+replaceitem@users.noreply.github.com>
2023-07-31 18:45:15 +0200

    Check whether client options are initialized (Fixes #20)

--- src/main/java/net/replaceitem/integratedcircuit/client/config/DefaultConfig.java
+++ src/main/java/net/replaceitem/integratedcircuit/client/config/DefaultConfig.java
@@ -2,8 +2,10 @@ package net.replaceitem.integratedcircuit.client.config;
 
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.util.InputUtil;
 import net.replaceitem.integratedcircuit.mixin.KeyBindingAccessor;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
 
@@ -22,13 +24,16 @@ public class DefaultConfig {
     }
 
     public InputUtil.Key getPlaceKeybind() {
-        return ((KeyBindingAccessor) MinecraftClient.getInstance().options.useKey).getBoundKey();
+        GameOptions options = getGameOptions();
+        return options == null ? InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_RIGHT) : ((KeyBindingAccessor) options.useKey).getBoundKey();
     }
     public InputUtil.Key getDestroyKeybind() {
-        return ((KeyBindingAccessor) MinecraftClient.getInstance().options.attackKey).getBoundKey();
+        GameOptions options = getGameOptions();
+        return options == null ? InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_LEFT) : ((KeyBindingAccessor) options.attackKey).getBoundKey();
     }
     public InputUtil.Key getPickKeybind() {
-        return ((KeyBindingAccessor) MinecraftClient.getInstance().options.pickItemKey).getBoundKey();
+        GameOptions options = getGameOptions();
+        return options == null ? InputUtil.Type.MOUSE.createFromCode(GLFW.GLFW_MOUSE_BUTTON_MIDDLE) : ((KeyBindingAccessor) options.pickItemKey).getBoundKey();
     }
     public InputUtil.Key getRotateKeybind() {
         return InputUtil.Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_R);
@@ -54,5 +59,13 @@ public class DefaultConfig {
         public String toString() {
             return prettyName;
         }
+    }
+    
+    
+    @Nullable
+    public static GameOptions getGameOptions() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if(client == null) return null;
+        return client.options;
     }
 }
