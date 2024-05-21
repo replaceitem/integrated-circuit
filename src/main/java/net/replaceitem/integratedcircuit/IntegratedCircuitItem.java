28f0750ab692207320c5d8ba4dc905d7ad4d9f73
replaceitem <40722305+replaceitem@users.noreply.github.com>
2024-05-21 15:34:40 +0200

    Fix name of empty circuits

--- src/main/java/net/replaceitem/integratedcircuit/IntegratedCircuitItem.java
+++ src/main/java/net/replaceitem/integratedcircuit/IntegratedCircuitItem.java
@@ -1,5 +1,6 @@
 package net.replaceitem.integratedcircuit;
 
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -11,7 +12,7 @@ public class IntegratedCircuitItem extends BlockItem {
 
     @Override
     public Text getName(ItemStack stack) {
-        if(!stack.getComponents().isEmpty()) return super.getName(stack);
+        if(stack.getComponents().contains(DataComponentTypes.BLOCK_ENTITY_DATA)) return super.getName(stack);
         return Text.translatable(this.getTranslationKey() + ".empty");
     }
 }
